87ec3e2619dc6aab63080a816886d135014991b6
piotr <p.kacprzak.96@gmail.com>
2020-03-21 12:43:44 +0100

    BinaryChromosome subtype

--- src/file1.py
+++ src/file1.py
@@ -2,59 +2,61 @@ import numpy as np
 
 
 class Chromosome:
-    def __init__(self, number_of_genes):
-        self.value = np.random.randint(0, 2, size=number_of_genes)
+    def show(self):
+        pass
+
+
+class BinaryChromosome(Chromosome):
+    @staticmethod
+    def calculate_chain_length(range_start, range_end, acc):
+        if range_start > range_end:
+            return "Wrong range"
+        else:
+            return np.math.ceil(np.math.log2(abs(range_end - range_start) * pow(10, acc)))
+
+    def __init__(self, genes_in_chr):
+        self.value = np.random.randint(0, 2, size=genes_in_chr)
+
+    def show(self):
+        print(self.value)
+
+    def decode_val_to_decimal(self):
+        return self.value
 
 
 class Individual:
-    def __init__(self, chromosomes_array):
-        self.value = np.asarray(chromosomes_array)
+    def __init__(self, chromosome_type, chr_in_indiv, genes_in_chr):
+        chromosomes = []
+        for i in range(chr_in_indiv):
+            chromosomes.append(chromosome_type(genes_in_chr))
+        self.chromosomes = np.asarray(chromosomes)
 
-    def decode_to_decimal(self):
-        return 0 # TODO
 
 class Population:
-    def __init__(self, array_of_individuals):
-        self.value = np.asarray(array_of_individuals)
+    def __init__(self, chromosome_type, pop_size, chr_in_indiv, genes_in_chr):
+        individuals = []
+        for i in range(pop_size):
+            individuals.append(Individual(chromosome_type, chr_in_indiv, genes_in_chr))
+        self.individuals = np.asarray(individuals)
 
-    def print(self):
-        for ind_val in self.value:
-            for chr_val in ind_val.value:
-                print(chr_val.value)
+    def print_population(self):
+        for individual in self.individuals:
+            for chromosome in individual.chromosomes:
+                chromosome.show()
             print(" ")
-
-
-def calculate_chain_length(range_start, range_end, acc):
-    if range_start > range_end:
-        print("Wrong range")
-    else:
-        return np.math.ceil(np.math.log2(abs(range_end - range_start) * pow(10, acc)))
-
-
-def generate_population(number_of_genes, chromosome_size, population_size):
-    population = []
-    for i in range(population_size):
-        chromosomes = []
-        for j in range(chromosome_size):
-            chromosomes.append(Chromosome(number_of_genes))
-        population.append(Individual(chromosomes))
-    return Population(population)
 
 
 a = -10
 b = 10
 accuracy = 6
 
-number_of_genes = calculate_chain_length(a, b, accuracy)
+number_of_genes = BinaryChromosome.calculate_chain_length(a, b, accuracy)
 
 chromosome_size = 1
 population_size = 5
 
-pop1 = generate_population(number_of_genes, chromosome_size, population_size)
-pop1.print()
+pop1 = Population(BinaryChromosome, population_size, chromosome_size, number_of_genes)
+pop1.print_population()
+
 
 epochs_number = 10
-
-
-
-
