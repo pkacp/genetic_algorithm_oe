6658c3b05c71fa6eea86adc179d64ad1dc710453
piotr <p.kacprzak.96@gmail.com>
2020-03-21 16:33:28 +0100

    Test n point crossing instead many others

--- src/BinaryChromosome.py
+++ src/BinaryChromosome.py
@@ -18,7 +18,8 @@ class BinaryChromosome(Chromosome):
         return int(str_bit_number, 2)
 
     def __init__(self, genes_in_chr):
-        self.value = np.random.randint(0, 2, size=genes_in_chr)
+        # self.value = np.random.randint(0, 2, size=genes_in_chr)
+        self.value = np.random.randint(0, 9, size=genes_in_chr)
 
     def show(self):
         print(self.value)
@@ -27,8 +28,37 @@ class BinaryChromosome(Chromosome):
         m = len(self.value)
         return range_start + self.binary_arr_to_int(self.value) * (range_end - range_start) / (pow(2, m) - 1)
 
-    def one_point_cross(self, other_binary_chromosome):
-        self_split = np.array_split(self.value, 2)
-        other_split = np.array_split(other_binary_chromosome.value, 2)
-        self.value = np.concatenate((self_split[0], other_split[1]))
-        other_binary_chromosome.value = np.concatenate((other_split[0], self_split[1]))
+    def n_point_cross(self, n, other_binary_chromosome):
+        self_split = np.array_split(self.value, n)
+        other_split = np.array_split(other_binary_chromosome.value, n)
+        print("-------------------")
+        print(self_split)
+        print(other_split)
+        self_array_of_splits = []
+        other_array_of_splits = []
+        print("...............")
+        for i in range(n):
+            if i % 2 == 0:
+                self_array_of_splits.append(self_split[i])
+                other_array_of_splits.append(other_split[i])
+            else:
+                self_array_of_splits.append(other_split[i])
+                other_array_of_splits.append(self_split[i])
+
+        print(self_array_of_splits)
+        print(other_array_of_splits)
+        print("-------------------")
+        self.value = np.concatenate(self_array_of_splits)
+        other_binary_chromosome.value = np.concatenate(other_array_of_splits)
+
+    # def one_point_cross(self, other_binary_chromosome):
+    #     self_split = np.array_split(self.value, 2)
+    #     other_split = np.array_split(other_binary_chromosome.value, 2)
+    #     self.value = np.concatenate((self_split[0], other_split[1]))
+    #     other_binary_chromosome.value = np.concatenate((other_split[0], self_split[1]))
+    #
+    # def two_point_cross(self, other_binary_chromosome):
+    #     self_split = np.array_split(self.value, 3)
+    #     other_split = np.array_split(other_binary_chromosome.value, 3)
+    #     self.value = np.concatenate((self_split[0], other_split[1], self_split[2]))
+    #     other_binary_chromosome.value = np.concatenate((other_split[0], self_split[1], other_split[2]))
--- src/main.py
+++ src/main.py
@@ -2,17 +2,27 @@ from src.BinaryChromosome import BinaryChromosome
 from src.Population import Population
 
 if __name__ == '__main__':
-    a = -10
-    b = 10
-    accuracy = 6
+    # a = -10
+    # b = 10
+    # accuracy = 6
+    #
+    # number_of_genes = BinaryChromosome.calculate_chain_length(a, b, accuracy)
+    #
+    # chromosome_size = 1
+    # population_size = 5
+    #
+    # pop1 = Population(BinaryChromosome, population_size, chromosome_size, number_of_genes)
+    # pop1.print_population()
+    # pop1.print_decimal_population(a, b)
+    #
+    # epochs_number = 10
 
-    number_of_genes = BinaryChromosome.calculate_chain_length(a, b, accuracy)
+    aa = BinaryChromosome(7)
+    bb = BinaryChromosome(7)
+    aa.show()
+    bb.show()
 
-    chromosome_size = 1
-    population_size = 5
-
-    pop1 = Population(BinaryChromosome, population_size, chromosome_size, number_of_genes)
-    pop1.print_population()
-    pop1.print_decimal_population(a, b)
-
-    epochs_number = 10
+    # aa.two_point_cross(bb)
+    aa.n_point_cross(4, bb)
+    aa.show()
+    bb.show()
